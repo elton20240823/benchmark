a82bff34e7d620287112efb6c8445cbdf845e99f
袁望 <yuanwang@avenir.hk>
2024-09-12 15:22:00 +0800

    refactor: 移除200b相关测试脚本

--- benchmark_phoenix_md.sh
+++ benchmark_phoenix_md.sh
@@ -11,9 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-bin/benchmark --drivers driver-kafka/kafka-throughput-md.yaml --workers http://localhost:21001,http://localhost:21003  workloads/phoenix-1-topic-1-partition-200b-r-112500.yaml
-bin/benchmark --drivers driver-kafka/kafka-throughput-md.yaml --workers http://localhost:21001,http://localhost:21003  workloads/phoenix-1-topic-1-partition-200b-r-37500.yaml
-bin/benchmark --drivers driver-kafka/kafka-throughput-md.yaml --workers http://localhost:21001,http://localhost:21003  workloads/phoenix-1-topic-1-partition-200b-r-75000.yaml
 bin/benchmark --drivers driver-kafka/kafka-throughput-md.yaml --workers http://localhost:21001,http://localhost:21003  workloads/phoenix-1-topic-1-partition-32kb-r-224.yaml
 bin/benchmark --drivers driver-kafka/kafka-throughput-md.yaml --workers http://localhost:21001,http://localhost:21003  workloads/phoenix-1-topic-1-partition-32kb-r-448.yaml
 bin/benchmark --drivers driver-kafka/kafka-throughput-md.yaml --workers http://localhost:21001,http://localhost:21003  workloads/phoenix-1-topic-1-partition-32kb-r-672.yaml
--- command.md
+++ command.md
@@ -2,6 +2,8 @@
 
 ## 启动worker
 ```shell
-~/benchmark/bin/benchmark-worker --port 21001 --stats-port 21002
-~/benchmark/bin/benchmark-worker --port 21003 --stats-port 21004
+cd ~/benchmark/
+bin/benchmark-worker --port 21001 --stats-port 21002
+cd ~/benchmark/
+bin/benchmark-worker --port 21003 --stats-port 21004
 ```
